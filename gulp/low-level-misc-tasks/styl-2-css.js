bdfce645d72314b5d7ab7cee3484d492808d4577
Dan Lewis <daniel.d.lewis@baesystems.com>
2016-06-22 11:37:33 +0100

    styl sourcemaps and nib introduced

--- gulp/low-level-misc-tasks/styl-2-css.js
+++ gulp/low-level-misc-tasks/styl-2-css.js
@@ -1,13 +1,20 @@
 const TASK = 'styl-2-css';
 const core = require('../../gulp-core');
 const stylus = require('gulp-stylus');
+const sourcemaps = require('gulp-sourcemaps');
+const nib = require('nib');
 
 core.gulp.task(TASK, () => {
   core.subHeading(TASK, 'Transpiling Stylus to CSS');
   return core.gulp
     .src(core.config.files.styl)
+    .pipe(sourcemaps.init())
     .pipe(stylus({
-      'include css': true
+      'include css': true,
+      compress: true,
+      use: nib()
     }))
+    .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../app'}))
     .pipe(core.gulp.dest('build'));
+
 });
